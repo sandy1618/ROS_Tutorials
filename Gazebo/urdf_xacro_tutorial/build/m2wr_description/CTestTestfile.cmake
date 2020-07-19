16bfcf09a2460ee083688960c6694eb453884433
sandy1618 <sandeepnayak1.618@gmail.com>
2020-07-19 20:18:30 +0900

    Added urdf_xacro_tutorial folder

---
+++ Gazebo/urdf_xacro_tutorial/build/m2wr_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sandy/Education/ROS_Tutorials/Gazebo/urdf_xacro_tutorial/src/m2wr_description
+# Build directory: /home/sandy/Education/ROS_Tutorials/Gazebo/urdf_xacro_tutorial/build/m2wr_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Gazebo/urdf_xacro_tutorial/build/m2wr_description_without_xacro/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sandy/Education/ROS_Tutorials/Gazebo/urdf_xacro_tutorial/src/m2wr_description_without_xacro
+# Build directory: /home/sandy/Education/ROS_Tutorials/Gazebo/urdf_xacro_tutorial/build/m2wr_description_without_xacro
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
